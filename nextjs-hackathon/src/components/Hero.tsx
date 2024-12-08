0aca9065bde03adba813e249413abee2d3cbc217
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-12-08 20:27:31 +0500

    first commit

---
+++ nextjs-hackathon/src/components/Hero.tsx
@@ -0,0 +1,22 @@
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+    weight: "500"
+})
+
+export default function Hero() {
+    return (
+        <div className="parent w-full flex 2xl:justify-center">
+            <div className="child h-[812.53px] bg-heroImg bg-repeat bg-cover bg-bottom w-full 2xl:w-[1440px] mx-auto flex items-center justify-end">
+                <div className="w-[350px] md:w-[643px] h-[620px] md:h-[443px] bg-[#FFF3E3] flex md:items-center justify-center mr-3 2xl:mr-14 md:mt-28">
+                    <div className={`${poppins.className} w-[268px] md:w-[561px] h-[344px] mt-5 md:mt-0`}>
+                        <p className="text-[16px] font-semibold">New Arrival</p>
+                        <h1 className="text-[52px] font-bold text-[#B88E2F]">Discover Our <br /> New Collection</h1>
+                        <p className="text-[18px] font-medium">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+                        <button className="w-[222px] h-[74px] bg-[#B88E2F] mt-14 text-white text-[16px] font-bold">BUY NOW</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ nextjs-hackathon/src/components/Navbar.tsx
@@ -0,0 +1,146 @@
+import Image from "next/image"
+import Link from "next/link"
+import { Poppins } from 'next/font/google'
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import { FiMenu } from "react-icons/fi";
+
+
+const poppins = Poppins({
+    weight: "500"
+})
+
+export default function Navbar() {
+    return (
+        <div className="parent w-full flex 2xl:justify-center">
+            <div className="child w-[95%] 2xl:w-[1440px] h-[100px] mx-auto bg-white flex items-center justify-between">
+                <div className="logo flex gap-3">
+                    <Image
+                        src={"/logo.svg"}
+                        alt="logo"
+                        width={50}
+                        height={32}
+                    />
+                    <Image
+                        src={"/logo2.svg"}
+                        alt="logo2"
+                        width={130}
+                        height={41}
+                    />
+                </div>
+
+                <div className="hidden lg:block">
+                    <ul className={`${poppins.className} text-[16px] font-medium flex gap-14`}>
+                        <li><Link href={"/"}>Home</Link></li>
+                        <li><Link href={"/shop"}>Shop</Link></li>
+                        <li><Link href={"/blog"}>Blog</Link></li>
+                        <li><Link href={"/contact"}>Contact</Link></li>
+                    </ul>
+                </div>
+
+                <div className="hidden lg:block">
+                    <div className="navIcons flex gap-10">
+                        <Image
+                            className="cursor-pointer"
+                            src={"vector1.svg"}
+                            alt="Vector1"
+                            width={23.33}
+                            height={18.67}
+                        />
+                        <Image
+                            className="cursor-pointer"
+                            src={"vector2.svg"}
+                            alt="Vector2"
+                            width={22.17}
+                            height={22.17}
+                        />
+                        <Image
+                            className="cursor-pointer"
+                            src={"vector3.svg"}
+                            alt="Vector3"
+                            width={23.33}
+                            height={20.81}
+                        />
+                        <Image
+                            className="cursor-pointer"
+                            src={"vector4.svg"}
+                            alt="Vector4"
+                            width={24.53}
+                            height={22.06}
+                        />
+                    </div>
+                </div>
+
+                <div className="lg:hidden">
+                    <Sheet>
+                        <SheetTrigger><FiMenu /></SheetTrigger>
+                        <SheetContent>
+                            <SheetHeader>
+                                <SheetTitle><div className="logo flex gap-3">
+                                    <Image
+                                        src={"/logo.svg"}
+                                        alt="logo"
+                                        width={50}
+                                        height={32}
+                                    />
+                                    <Image
+                                        src={"/logo2.svg"}
+                                        alt="logo2"
+                                        width={130}
+                                        height={41}
+                                    />
+                                </div></SheetTitle>
+                                <SheetDescription>
+                                    <ul className={`${poppins.className} text-[26px] font-medium flex flex-col gap-10 mt-10`}>
+                                        <li><Link href={"/"}>Home</Link></li>
+                                        <li><Link href={"/shop"}>Shop</Link></li>
+                                        <li><Link href={"/blog"}>Blog</Link></li>
+                                        <li><Link href={"/contact"}>Contact</Link></li>
+                                    </ul>
+
+                                    <div className="navIcons flex flex-col mt-10 gap-10">
+                                        <Image
+                                            className="cursor-pointer"
+                                            src={"vector1.svg"}
+                                            alt="Vector1"
+                                            width={23.33}
+                                            height={18.67}
+                                        />
+                                        <Image
+                                            className="cursor-pointer"
+                                            src={"vector2.svg"}
+                                            alt="Vector2"
+                                            width={22.17}
+                                            height={22.17}
+                                        />
+                                        <Image
+                                            className="cursor-pointer"
+                                            src={"vector3.svg"}
+                                            alt="Vector3"
+                                            width={23.33}
+                                            height={20.81}
+                                        />
+                                        <Image
+                                            className="cursor-pointer"
+                                            src={"vector4.svg"}
+                                            alt="Vector4"
+                                            width={24.53}
+                                            height={22.06}
+                                        />
+                                    </div>
+                                </SheetDescription>
+                            </SheetHeader>
+                        </SheetContent>
+                    </Sheet>
+                </div>
+            </div>
+
+        </div>
+    )
+}
